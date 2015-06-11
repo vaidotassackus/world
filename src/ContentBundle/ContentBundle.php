96d898acc0a772e62e7a8d9976d3b0cfc0208483
VaidotasSackus <vaidotas.sackus@gmail.com>
2015-06-11 19:58:21 +0300

    First commit

---
+++ src/ContentBundle/ContentBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ContentBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class ContentBundle
+ * @package ContentBundle
+ */
+class ContentBundle extends Bundle
+{
+}
---
+++ src/ContentBundle/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ContentBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Class DefaultController
+ * @package ContentBundle\Controller
+ */
+class DefaultController extends Controller
+{
+    /**
+     * Index function.
+     * @return array
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $name = 'Laba diena';
+
+        return ['name' => $name];
+    }
+}
